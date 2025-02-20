41939a1a5671ea00c87b5106a29af145b8994bf8
gcuiza <gualbertocuizav@gmail.com>
2025-02-20 09:17:10 -0400

    trend cats

--- src/cats/components/Cats.vue
+++ src/cats/components/Cats.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import api from "@/api";
 import Cat from "./Cat.vue";
 import { onMounted, ref } from "vue";
 import { useCatStore } from "../store/useCat";
@@ -8,7 +7,7 @@ import * as CatService from "../services/catService";
 
 const filters = [
   { label: "all", value: "recent" },
-  { label: "voted", value: "voted" },
+  { label: "most favorites", value: "voted" },
 ];
 
 const catStore = useCatStore();
@@ -25,6 +24,13 @@ const getCats = async () => {
   catStore.setCats(cats);
 };
 
+const getTrendiestCats = async () => {
+  const cats = await CatService.getCats(
+    `skip=${catStore.cats.length}&limit=10&tags=supercat,supermodel`
+  );
+  catStore.setCats(cats);
+};
+
 const handleFilter = () => {
   console.log(selectedFilter.value);
   catStore.sortCats(selectedFilter.value);
@@ -33,21 +39,24 @@ const handleFilter = () => {
 
 <template>
   <div>
-    <div class="flex gap-2 mt-5 items-center">
-      <span class="tracking-widest">Sort By:</span>
-      <select
-        v-model="selectedFilter"
-        @change="handleFilter"
-        class="p-2 border border-gray-300 rounded-lg"
-      >
-        <option
-          v-for="filter in filters"
-          :key="filter.value"
-          :value="filter.value"
+    <div class="flex justify-between items-center">
+      <div class="flex gap-2 mt-5 items-center">
+        <span class="tracking-widest">Sort By:</span>
+        <select
+          v-model="selectedFilter"
+          @change="handleFilter"
+          class="p-2 border border-gray-300 rounded-lg"
         >
-          {{ filter.label }}
-        </option>
-      </select>
+          <option
+            v-for="filter in filters"
+            :key="filter.value"
+            :value="filter.value"
+          >
+            {{ filter.label }}
+          </option>
+        </select>
+      </div>
+      <Button @click="getTrendiestCats" disabled> Trendiest Cats </Button>
     </div>
     <div class="grid grid-cols-5 gap-10">
       <Cat v-for="cat in catStore.cats" :key="cat.id" :cat="cat" />
